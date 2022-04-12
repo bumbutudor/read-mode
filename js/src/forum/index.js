c9c1a70974d0b28c61f7a2be070bccd0891ccc53
bumbutudor <bumbutudor10@gmail.com>
2022-04-12 14:56:27 +0300

    add mozilla readability
    add getBrowser utility

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -7,26 +7,53 @@ import Discussion from 'flarum/common/models/Discussion';
 
 import ReadModeModal from './components/ReadModeModal';
 import ArticleModal from './components/ArticleModal';
+import { getBrowser } from './utils/getBrowser';
+import { isProbablyReaderable, Readability } from '@mozilla/readability'
 
 app.initializers.add('tudor/read-mode', () => {
-  
-  // Discussion.prototype.shareUrl = Model.attribute('shareUrl');
 
+  // Discussion.prototype.shareUrl = Model.attribute('shareUrl');
   extend(DiscussionPage.prototype, 'sidebarItems', function (items) {
-      items.add('read-mode',
-        <Button
-          class="Button Button-icon Button--read-mode"
-          icon="fas fa-book-reader"
-          onclick={() =>
-            app.modal.show(ArticleModal, {
-              discussion: this.discussion,
-            })
+    const browser = getBrowser();
+
+    console.log(browser);
+    // fnBrowserDetect();
+
+    items.add('read-mode',
+      <Button
+        class="Button Button-icon Button--read-mode"
+        icon="fas fa-book-reader"
+        onclick={() => {
+          if (browser === 'chrome') {
+            app.modal.show(ArticleModal, { discussion: this.discussion });
           }
-        >
-          {app.translator.trans('read-mode.forum.discussion.read_mode_button')}
-        </Button>,
-        -1
-      );
+          if (browser === 'firefox') {
+            if (isProbablyReaderable(document)) {
+              // var documentClone = document.cloneNode(true);
+              // var article = new Readability(documentClone).parse();
+              document.dispatchEvent(
+                new KeyboardEvent("keydown", {
+                  "key": "F9",
+                  "keyCode": 120,
+                  "which": 120,
+                  "code": "F9",
+                  "location": 0,
+                  "altKey": false,
+                  "ctrlKey": false,
+                  "metaKey": false,
+                  "shiftKey": false,
+                })
+              );
+            }
+          }
+        }
+
+        }
+      >
+        {app.translator.trans('read-mode.forum.discussion.read_mode_button')}
+      </Button>,
+      -1
+    );
   });
 
 });
